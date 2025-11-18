9da3f1952bc11be1b17fdc4a4208edad88e54221
OliviaDepatie <depatie@hartford.edu>
2025-11-18 03:49:43 -0500

    Update RunMatrix.java

--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -1,6 +1,17 @@
 import java.util.Scanner;
 
 public class RunMatrix {
-  public static void main(String[] args) {
-  }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter the size of your matrix: ");
+        int size = scan.nextInt();
+        scan.close();
+
+        Matrix matrix = new Matrix(size);
+        matrix.printMatrixDefault();
+        matrix.populateMatrix();
+        matrix.printMatrix("Printing matrix:");
+        matrix.flipMatrix();
+        matrix.printMatrix("Printing flipped matrix:");
+    }
 }
